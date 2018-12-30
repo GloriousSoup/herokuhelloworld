7eefcf804691995d68749608fe796c401c805cb7
Tony Porteous <tony.porteous@gmail.com>
2018-12-30 15:07:41 +0000

    Added a test test

--- test/models/admin_user_test.rb
+++ test/models/admin_user_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AdminUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "creation" do
+    c = AdminUser.new
+    c.email = "user@example.com"
+    c.password = "password"
+    assert c.save
+  end
 end
